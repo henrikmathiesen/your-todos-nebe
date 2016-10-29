6e774e79aec598a2d33e09ac7824819dc16f84b6
Henrik <henrik_mathiesen@hotmail.com>
2016-10-29 09:20:05 +0200

    -gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,21 @@
-// todo
+var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
+
+var isProduction = (argv.prod) ? (true) : (false);
+var resetInject = (argv.resetinject) ? (true) : (false);
+
+var config = {
+    src: {
+        app: ['./app/**/*.module.js', './app/**/*.js'],
+        lib: [
+            './node_modules/jquery/dist/jquery.js', 
+            './node_modules/fastclick/lib/fastclick.js', 
+            './node_modules/angular/angular.js', 
+            './node_modules/angular-mocks/angular-mocks.js'
+        ]
+    },
+    bld: './bld',
+    server: {
+
+    }
+}
