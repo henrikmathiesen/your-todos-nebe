3323db0b5439f3cc74382302a796e852538f6db1
Henrik <henrik_mathiesen@hotmail.com>
2016-10-29 08:19:46 +0200

    -karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,2 +1,20 @@
 /// <reference path="./typings/tsd.d.ts" />
 
+module.export = function (config) {
+
+    config.set({
+        browsers: ['PhantomJS'],
+        frameworks: ['jasmine'],
+        files: [
+            // angular-mocks is included in lib since we are using $httpBackend for backend less development
+            './bld/lib.js',
+            './bld/app-templates.js',
+            './bld/app.js',
+            './tst/**/*.js'
+        ],
+        logLevel: config.LOG_DISABLE,
+        singleRun: true,
+        autoWatch: false
+    });
+
+}
