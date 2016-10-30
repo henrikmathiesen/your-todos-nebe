db8d64db02445981822324db535c5d93088f16fa
Henrik <henrik_mathiesen@hotmail.com>
2016-10-30 15:45:39 +0100

    -api factory

---
+++ app/todos/api.factory.js
@@ -0,0 +1,27 @@
+/// <reference path="../../typings/index.d.ts" />
+
+angular
+    .module('todos')
+    .factory('apiFactory', function ($http) {
+
+        var factory = {};
+
+        factory.getTodos = function () {
+            return $http.get('/api/todos');
+        };
+
+        factory.postTodo = function (todo) {
+            return $http.post('/api/todo', todo);
+        };
+
+        factory.putTodo = function (id, todo) {
+            return $http.put('/api/todo/' + id, todo);
+        };
+
+        factory.deleteTodo = function (id) {
+            return $http.delete('/api/todo/' + id);
+        };
+
+        return factory;
+
+    });
