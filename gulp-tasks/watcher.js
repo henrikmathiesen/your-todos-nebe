c25ea0fc20bfe7dae1da645478fa051580d8a7e1
Henrik <henrik_mathiesen@hotmail.com>
2016-10-30 13:36:30 +0100

    -watch and inject on indexhtml changes -design labbing

--- gulp-tasks/watcher.js
+++ gulp-tasks/watcher.js
@@ -3,5 +3,6 @@ module.exports = function (gulp, config) {
         gulp.watch(config.src.app, ['jsapp']);
         gulp.watch(config.src.templates, ['template-cache']);
         gulp.watch(config.src.lessWatch, ['less']);
+        gulp.watch(config.src.indexhtml, ['watch-indexhtml-task']);
     };
 };
