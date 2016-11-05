00e623cdcebc581d651ba8ddd75e3e3ef051232f
Henrik <henrik_mathiesen@hotmail.com>
2016-11-05 19:23:49 +0100

    -comments about unit testing our backend

---
+++ tst/backend/backend.factory.test.js
@@ -0,0 +1,5 @@
+/// <reference path="../../typings/index.d.ts" />
+
+// Todo
+// Remember, can not include backend module (which depends on ngMockE2E) in a unit test
+// need to mock that module some way and test the factory
