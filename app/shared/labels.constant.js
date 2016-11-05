ef13c83f96d8cf4b07d049e90eadc7e537d19346
Henrik <henrik_mathiesen@hotmail.com>
2016-11-05 13:17:57 +0100

    -label constant

---
+++ app/shared/labels.constant.js
@@ -0,0 +1,9 @@
+/// <reference path="../../typings/index.d.ts" />
+
+angular
+    .module('shared')
+    .constant('LABELS_CONSTANTS', {
+        'work': 'work',
+        'joy': 'joy',
+        'project': 'project'
+    });
