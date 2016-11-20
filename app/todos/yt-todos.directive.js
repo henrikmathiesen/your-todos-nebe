1857f63fd552cb983834969b0a63dbe9fe0dac96
Henrik <henrik_mathiesen@hotmail.com>
2016-11-20 15:10:22 +0100

    -delete, check todos

--- app/todos/yt-todos.directive.js
+++ app/todos/yt-todos.directive.js
@@ -17,6 +17,21 @@ angular
         vm.todos = [];
         vm.allTodosChecked = false;
 
+        vm.isAllTodosChecked = function () {
+            if(!vm.todos.length) {
+                vm.allTodosChecked = false;
+                vm.noTodosChecked = true;
+                return;
+            }
+
+            vm.allTodosChecked = vm.todos.every(function (todo) { return todo.checked; });
+            vm.noTodosChecked = !vm.todos.some(function (todo) { return todo.checked; });
+        };
+
+        vm.checkAllTodos = function (isChecked) {
+            vm.todos.map(function (todo) { todo.checked = isChecked; });
+            vm.isAllTodosChecked();
+        };
 
         var getTodos = function () {
             console.log("one time");
@@ -25,28 +40,20 @@ angular
                 .then(function (todos) {
                     vm.todos = todos;
                     vm.todos.map(function (todo) { todo.checked = false; });
+                    vm.isAllTodosChecked();
                 });
-        };
-
-        vm.checkAllTodos = function (isChecked) {
-            vm.todos.map(function (todo) { todo.checked = isChecked; });
-            vm.allTodosChecked = isChecked;
-        };
-
-        vm.isAllTodosChecked = function () {
-            vm.allTodosChecked = vm.todos.every(function (todo) { return todo.checked; });
         };
 
         vm.deleteCheckedTodos = function () {
             var promises = [];
-            
-            for (var i = 0; i < vm.todos.length; i++) { 
+
+            for (var i = 0; i < vm.todos.length; i++) {
                 if (vm.todos[i].checked === true) {
                     var promise = todosCrudFactory.deleteTodo(vm.todos[i].id);
                     promises.push(promise);
                 }
             }
-            
+
             $q.all(promises).then(getTodos);
         };
 
