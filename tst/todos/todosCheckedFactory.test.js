91e909d623379685116f259efbc2fe7f78e2921f
Henrik <henrik_mathiesen@hotmail.com>
2016-11-27 13:03:59 +0100

    -unit tests

--- tst/todos/todosCheckedFactory.test.js
+++ tst/todos/todosCheckedFactory.test.js
@@ -3,6 +3,7 @@
 describe("todosChecked.factory keeps track of checked todos and can delete them by forward calls to todosCrud.factory", function () {
 
     var $q;
+    var $scope;
     var todosCheckedFactory;
     var todosCrudFactory;
     var todos;
@@ -11,8 +12,9 @@ describe("todosChecked.factory keeps track of checked todos and can delete them 
     beforeEach(module('shared'));
     beforeEach(module('todos'));
 
-    beforeEach(inject(function (_$q_, _todosCheckedFactory_, _todosCrudFactory_) {
+    beforeEach(inject(function (_$q_, _$rootScope_, _todosCheckedFactory_, _todosCrudFactory_) {
         $q = _$q_;
+        $scope = _$rootScope_;
         todosCheckedFactory = _todosCheckedFactory_;
         todosCrudFactory = _todosCrudFactory_;
 
@@ -99,6 +101,57 @@ describe("todosChecked.factory keeps track of checked todos and can delete them 
 
             expect(vm.allTodosChecked).toBe(false);
             expect(vm.noTodosChecked).toBe(true);
+        });
+
+    });
+
+    describe("there should be a function for deleting checked todos, by calling todosCrudFactory, then fades them out and call getTodos callback", function () {
+
+        it("should delete checked todos, fade out them and then call reload callback", function () { 
+            vm.todos = todos;
+            vm.todos[0].checked = false;
+            vm.todos[1].checked = true;
+            vm.todos[2].checked = true;
+            var obj = { reload: function () { return true; } };
+            
+            spyOn(obj, 'reload');
+            spyOn(angular, 'forEach');
+            spyOn(todosCrudFactory, 'deleteTodo').and.callFake(function(){
+                // return $q.defer().promise; - since $q.all(promises).then() runs when all promises are resolved, we can not return $q.defer().promise
+                // We need to return a resolved promise and then $scope.$digest() to test the next action
+
+                var deferred = $q.defer();
+                deferred.resolve("204");
+                return deferred.promise;
+            });
+
+            todosCheckedFactory.deleteCheckedTodos(vm, obj.reload);
+
+            expect(todosCrudFactory.deleteTodo).toHaveBeenCalledTimes(2, "id 2 and id 3 is sent for deletion with 2 calls to crudFactory");
+            
+            $scope.$digest();
+            expect(angular.forEach).toHaveBeenCalled(); // fades out element
+            expect(obj.reload).toHaveBeenCalled();
+        });
+
+        it("should not enter q.all():s then() callback that fades out todos and reloads todos, if deletion gives ajax error", function () {
+            vm.todos = todos;
+            vm.todos[0].checked = false;
+            vm.todos[1].checked = true;
+            vm.todos[2].checked = false;
+            var obj = { reload: function () { return true; } };
+            
+            spyOn(obj, 'reload');
+            spyOn(angular, 'forEach');
+            spyOn(todosCrudFactory, 'deleteTodo').and.returnValue($q.reject());         // $q.reject() Returns a promise that was already resolved as rejected with the reason
+
+            todosCheckedFactory.deleteCheckedTodos(vm, obj.reload);
+
+            expect(todosCrudFactory.deleteTodo).toHaveBeenCalledWith(2, "id 2 is sent for deletion with call to crudFactory but there is an ajax error");
+
+            $scope.$digest();
+            expect(angular.forEach).not.toHaveBeenCalled();
+            expect(obj.reload).not.toHaveBeenCalled();
         });
 
     });
