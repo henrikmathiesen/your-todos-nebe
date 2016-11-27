bae5ce2a469b6eb28393a4d7869a9b95d01c1ee1
Henrik <henrik_mathiesen@hotmail.com>
2016-11-27 10:52:51 +0100

    -removed date and label from backEndFactory.test, since they are not used -todosCheckedFactory.test begun

--- tst/backend/backend.factory.test.js
+++ tst/backend/backend.factory.test.js
@@ -16,44 +16,30 @@ describe("backend.factory supports backend-less module to support CRUD operation
                 var todos = [
                     {
                         id: 1,
-                        date: "2010-08-01T15:00:00+02:00",
-                        label: "work",
                         text: "Have skills that are in demand"
                     },
                     {
                         id: 2,
-                        date: "2010-08-01T15:00:00+02:00",
-                        label: "project",
                         text: "Assemble a tshirt gun"
                     },
                     {
                         id: 3,
-                        date: "2010-08-01T15:00:00+02:00",
-                        label: "no label",
                         text: "Make sure there are an equal ammount of blue berries in each muffin"
                     },
                     {
                         id: 4,
-                        date: "2010-08-01T15:00:00+02:00",
-                        label: "joy",
                         text: "Juggle and fail"
                     },
                     {
                         id: 5,
-                        date: "2010-08-01T15:00:00+02:00",
-                        label: "work",
                         text: "Study Angular"
                     },
                     {
                         id: 6,
-                        date: "2010-08-01T15:00:00+02:00",
-                        label: "work",
                         text: "Study React"
                     },
                     {
                         id: 7,
-                        date: "2010-08-01T15:00:00+02:00",
-                        label: "no label",
                         text: "Lorem ipsum dolores"
                     },
                 ];
--- tst/todos/todosCheckedFactory.test.js
+++ tst/todos/todosCheckedFactory.test.js
@@ -1,1 +1,19 @@
-// todos
+/// <reference path="../../typings/index.d.ts" />
+
+describe("todosChecked.factory keeps track of checked todos and can delete them by forward calls to todosCrud.factory", function () { 
+
+    var $q;
+    var todosCheckedFactory;
+    var todosCrudFactory;
+
+    beforeEach(module('todos'));
+
+    beforeEach(inject(function (_$q_, _todosCheckedFactory_, _todosCrudFactory_) {
+        $q = _$q_;
+        todosCheckedFactory = _todosCheckedFactory_;
+        todosCrudFactory = _todosCrudFactory_;
+    }));
+
+    
+
+});
