3ae2d9138ef532f0732ae483f9645de3e4c5320d
Henrik <henrik_mathiesen@hotmail.com>
2016-12-02 18:18:16 +0100

    -unit test

--- tst/todos/yt-todos.directive.test.js
+++ tst/todos/yt-todos.directive.test.js
@@ -5,10 +5,12 @@ describe("yt-todos.directive loads all todos, keeps tracks of if all or none tod
     var $scope;
     var $compile;
     var $q;
+    var $controller;
     var directiveMarkup;
     var directiveElement;
     var jQelement;
     var html;
+    var vm;
     var todosCrudFactory;
     var todosCheckedFactory;
 
@@ -17,10 +19,11 @@ describe("yt-todos.directive loads all todos, keeps tracks of if all or none tod
     beforeEach(module('shared'));
     beforeEach(module('todos'));
 
-    beforeEach(inject(function (_$rootScope_, _$compile_, _$q_, _todosCrudFactory_, _todosCheckedFactory_) {
+    beforeEach(inject(function (_$rootScope_, _$compile_, _$q_, _$controller_, _todosCrudFactory_, _todosCheckedFactory_) {
         $scope = _$rootScope_.$new();
         $compile = _$compile_;
         $q = _$q_;
+        $controller = _$controller_;
         todosCrudFactory = _todosCrudFactory_;
         todosCheckedFactory = _todosCheckedFactory_;
 
@@ -58,22 +61,64 @@ describe("yt-todos.directive loads all todos, keeps tracks of if all or none tod
         });
 
         it("Should check if all or none todo is checked", function () {
-            expect(todosCheckedFactory.isAllTodosChecked).toHaveBeenCalled();
+            expect(todosCheckedFactory.isAllTodosChecked).toHaveBeenCalledWith(jQelement.isolateScope().vm);
         });
 
-        it("Should have populated todos on view model", function(){
+        it("Should have populated todos on view model", function () {
             expect(jQelement.isolateScope().vm.todos.length).toBe(2);
         });
 
-        it("Should set all todos checked property to false", function(){
+        it("Should set all todos checked property to false", function () {
             expect(jQelement.isolateScope().vm.todos[0].checked).toBe(false);
             expect(jQelement.isolateScope().vm.todos[1].checked).toBe(false);
         });
 
+        it("Should have properties allTodosChecked and noTodosChecked on vm", function(){
+            expect(jQelement.isolateScope().vm.allTodosChecked).toBe(false);
+            expect(jQelement.isolateScope().vm.noTodosChecked).toBe(true);
+        });
+
+        it("Should have the view model bound to its controller", function () {
+            vm = $controller('ytTodosController');
+            $scope.$apply();
+
+            expect(vm.todos.length).toBe(2);
+
+            // We can get to the vm either with isolateScope (above) or by its controller like this
+        });
+
     });
 
-    describe("Directives view model has methods for handling checked todos", function(){
-        
+    describe("Directives view model has methods for handling checked todos, forwards call to todosCheckedFactory", function () {
+
+        beforeEach(function(){
+            vm = $controller('ytTodosController');
+            $scope.$apply();
+
+            spyOn(todosCheckedFactory, 'checkAllTodos');
+            spyOn(todosCheckedFactory, 'deleteCheckedTodos');
+        });
+
+        it("Should have a method for checking if all todos is checked", function () {
+            vm.isAllTodosChecked();
+
+            expect(todosCheckedFactory.isAllTodosChecked).toHaveBeenCalledWith(vm);
+        });
+
+        it("Should have a method for checking all todos", function () {
+            vm.checkAllTodos(true);
+
+            expect(todosCheckedFactory.checkAllTodos).toHaveBeenCalledWith(vm, true);
+        });
+
+        it("Should have a method for deleting all checked todos, forwarding the call and then reload todos", function () {
+            // We have already tested that todosCheckedFactory.deleteCheckedTodos runs the sent callback, so dont need to test it here
+
+            var getTodos = function(){ return true; };
+            vm.deleteCheckedTodos();
+
+            expect(todosCheckedFactory.deleteCheckedTodos).toHaveBeenCalled(); // with vm, getTodos (having problem mocking private var)
+        });
     });
 
 });
