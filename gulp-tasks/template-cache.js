d41f3716c9c0e61fb99517956d6aca9b44b7d73f
Henrik <henrik_mathiesen@hotmail.com>
2016-12-25 14:17:39 +0100

    -cache bust template file in prod

--- gulp-tasks/template-cache.js
+++ gulp-tasks/template-cache.js
@@ -1,11 +1,14 @@
 var uglify = require('gulp-uglify');
 var templateCache = require('gulp-angular-templatecache');
+var rev = require('gulp-rev');
+var gulpif = require('gulp-if');
 
 module.exports = function (gulp, config) {
     return function () {
         return gulp
             .src(config.src.templates)
             .pipe(templateCache('templates.js', { module: 'templatecache', standalone: true }))
+            .pipe(gulpif(config.isProduction, rev()))
             .pipe(gulp.dest(config.bld));
     };
 };
